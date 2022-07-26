180c20137f00849c2fac2d7bf775b817b4207a91
MPhillips21 <86633797+MPhillips21@users.noreply.github.com>
2022-07-26 02:04:10 +0000

    7 Moment

--- app/src/main/java/classscheduler/App.java
+++ app/src/main/java/classscheduler/App.java
@@ -23,7 +23,8 @@ public class App {
         blockOfClass = yma.nextInt();
         yma.nextLine();
       } catch (InputMismatchException notAnInt) {
-        System.out.println("This should be an integer.");
+        System.out.println("\n" + "This should be an integer.");
+        yma.nextLine();
         continue;
       }
       System.out.println("\n" + "Name:");
@@ -38,49 +39,54 @@ public class App {
     Schedule schedule = new Schedule(classes, new Class(0, "empty", "none"));
 
     while (true) {
-      System.out.println("Enter a command (add, drop, viewSchedule, viewAvailable): ");
-      yma.nextLine();
+      System.out.println("\n" + "Enter a command (add, drop, viewSchedule, viewAvailable): ");
       switch (yma.nextLine()) {
         case "add":
-          System.out.println("Enter Class ID: ");
+          System.out.println("\n" + "Enter Class ID: ");
           try {
             chosenID = yma.nextInt();
             yma.nextLine();
           } catch (InputMismatchException bad) {
-            System.out.println("Not a valid ID");
-            continue;
+            System.out.println("\n" + "Not a valid ID");
+            yma.nextLine();
+            break;
           }
           if ((chosenID > -1) && (chosenID < numberOfClasses)) {
             schedule.addClassToSchedule(chosenID);
           } else {
-            System.out.println("Not a valid ID");
-            continue;
+            System.out.println("\n" + "Not a valid ID");
+            yma.nextLine();
+            break;
           }
           break;
 
         case "drop":
-          System.out.println("Enter block: ");
+          System.out.println("\n" + "Enter block: ");
           try {
             chosenBlock = yma.nextInt();
             yma.nextLine();
           } catch (InputMismatchException bad) {
-            System.out.println("Not a valid block");
-            continue;
+            System.out.println("\n" + "Not a valid block");
+            yma.nextLine();
+            break;
           }
-          if ((chosenBlock > -1) && (chosenBlock < numberOfClasses)) {
+          if ((chosenBlock > 0) && (chosenBlock < 5)) {
             schedule.dropClass(chosenBlock);
           } else {
-            System.out.println("Not a valid block");
-            continue;
+            System.out.println("\n" + "Not a valid block");
+            yma.nextLine();
+            break;
           }
           break;
 
         case "viewSchedule":
           schedule.print();
+          break;
         case "viewAvailable":
           classes.print();
+          break;
         default:
-          System.out.println("Pick an option");
+          System.out.println("\n" + "Pick an option");
       }
     }
 
